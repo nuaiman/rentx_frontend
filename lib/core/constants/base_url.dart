0818c67e0e59352176dea014a5811dbbfae3b586
Nuaiman Ashiq <ashiqnuaiman@gmail.com>
2025-09-23 00:06:43 +0600

    commit

--- lib/core/constants/base_url.dart
+++ lib/core/constants/base_url.dart
@@ -1,3 +1,4 @@
 // const String baseUrl = 'http://localhost:8080';
-const String baseUrl = 'http://72.60.129.140:8080 ';
+// const String baseUrl = 'http://72.60.129.140:8080';
+const String baseUrl = 'https://ztkautomation.cloud';
 // const String baseUrl = 'https://rentx-backend-eyk6.onrender.com';
